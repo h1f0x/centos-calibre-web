8b724e50de3fd362b42f34b6c5ae01eae3ef11ae
h1f0x <9852011+h1f0x@users.noreply.github.com>
2019-08-22 09:39:42 +0200

    minor fixes and compose file added

--- rootfs/usr/bin/prepare-config.sh
+++ rootfs/usr/bin/prepare-config.sh
@@ -10,5 +10,9 @@ if [ ! -f /config/calibre-web/app.db ]; then
     cp -r /defaults/calibre-web/* /config/calibre-web/
 fi
 
+if [ ! -f /books/metadata.db ]; then
+    cp -r /defaults/calibre/metadata.db /books/
+fi
+
 ln -sf /config/calibre-web/app.db /opt/calibre-web-0.6.4/app.db
 ln -sf /config/calibre-web/gdrive.db /opt/calibre-web-0.6.4/gdrive.db
